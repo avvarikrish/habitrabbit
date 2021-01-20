6008ee75e7cf5d944e9a11902bc3fdf2d049b091
avvarikrish <avvarikrish@gmail.com>
2021-01-19 21:27:27 -0800

    Mongo start and connect

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import os
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/habitrabbit"
 load_dotenv()
+
+hrDB = PyMongo(app)
 
 @app.route('/')
 def hello():
+    users = hrDB.db["users"].find()
+    for user in users:
+        print(user, "Hello")
+
     return "Hello World!"
 
 if __name__ == '__main__':
