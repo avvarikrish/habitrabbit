7f9514fb9132881959673f3e0b3c7d52f774d9c9
avvarikrish <avvarikrish@gmail.com>
2021-01-20 00:50:20 -0800

    Added create-user endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,30 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
+import json
 import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/habitrabbit"
 load_dotenv()
 
-hrDB = PyMongo(app)
+hr_db = PyMongo(app)
+users_collection = hr_db.db["users"]
 
-@app.route('/')
-def hello():
-    users = hrDB.db["users"].find()
-    for user in users:
-        print(user, "Hello")
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    user_info = request.json
+    result = users_collection.update_one(
+        {"email": user_info["email"]},
+        {"$setOnInsert": user_info},
+        upsert=True
+    )
 
-    return "Hello World!"
+    # email already exists
+    if result.matched_count > 0:
+        return Response("email already exists: " + user_info["email"], 409)
+
+    return "success"
 
 if __name__ == '__main__':
     app.run()
