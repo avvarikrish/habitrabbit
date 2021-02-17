49969e10e376b8b78ca4421e4e9620b20bb68a0d
ag <ag@whiznets.com>
2021-02-16 22:36:39 -0800

    app_factory update

--- backend/app_factory.py
+++ backend/app_factory.py
@@ -1,4 +1,15 @@
 from app import app
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+from user_routes import user_api
+from score_routes import scores_api
+from index_routes import index_api
 
 def create_app():
+    app.config['MONGO_URI'] = 'mongodb://localhost:27017/habitrabbit'
+    app.register_blueprint(user_api, url_prefix='/users')
+    app.register_blueprint(scores_api, url_prefix='/scores')
+    app.register_blueprint(index_api, url_prefix='/index')
     return app
