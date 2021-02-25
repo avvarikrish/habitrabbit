fe787825ee41e3c77106be0734a3518b530fed2f
avvarikrish <avvarikrish@gmail.com>
2021-02-24 23:08:21 -0800

    Print url

--- backend/index_creation.py
+++ backend/index_creation.py
@@ -7,6 +7,7 @@ from pkg import truncate
 PRECISION = 4
 LAT_LONG_FILE = 'us-zip-code-latitude-and-longitude.json'
 URL = os.environ.get('SERVER_URL')
+print('URL', URL)
 
 with open(LAT_LONG_FILE, 'r') as data_file:
     data = json.load(data_file)
